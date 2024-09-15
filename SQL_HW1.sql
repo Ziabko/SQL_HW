6aebd4f8d9ea6d03588fcefbc9fe06b7c7e40ec0
Ziabko <146750145+Ziabko@users.noreply.github.com>
2024-09-15 19:05:34 +0200

    Add files via upload

---
+++ SQL_HW1.sql
@@ -0,0 +1,260 @@
+create database homeworks;
+
+use homeworks;
+
+create table staff(
+id int primary key auto_increment,
+firstname varchar(64) not null,
+lastname varchar(64) not null,
+position varchar(64),
+age int check (age >= 0 AND age <= 110),
+has_child char(1) default 'N',
+username varchar(64) unique
+);
+
+
+
+insert into staff (firstname, lastname, position, age, has_child, username)
+values('Anna', 'Schwarz', 'Ingenieurin', 35, 'Y', 'Ann_Sch');
+
+insert into staff (firstname, lastname, position, age, has_child, username)
+values('John', 'Smith', 'Ingenieur', 48, 'Y', 'Jo');
+
+insert into staff (firstname, lastname, position, age, has_child, username)
+values('Milana', 'Arste', 'Ärztin', 55, 'Y', 'Mila');
+
+insert into staff (firstname, lastname, position, age, has_child, username)
+values('Greg', 'Haus', 'Arzt', 52, 'N', 'HausG');
+
+insert into staff (firstname, lastname, position, age, has_child, username)
+values('Jack', 'Doson', 'Reparieur', 29, 'Y', 'Titanik');
+
+select * from staff;
+
+SET SQL_SAFE_UPDATES = 0;
+
+truncate table staff;
+
+DROP DATABASE homeworks;
+
+create database Staff;
+
+use Staff;
+
+
+CREATE TABLE Staff (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(64) NOT NULL,
+    lastname VARCHAR(64) NOT NULL,
+    position VARCHAR(128),
+    age INTEGER,
+    has_child CHAR(1),
+    username VARCHAR(128) unique
+  );
+
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Anna'
+        , 'Daniels'
+        , 'Senior Teacher'
+        , 22
+        , 'N'
+        , 'annakhach5'
+        );
+        
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Tom'
+        , 'Austin'
+        , 'Junior Blogger'
+        , 25
+        , 'Y'
+        , 'tom12345'
+        );
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Bill'
+        , 'Lorentz'
+        , 'Junior Web Developer'
+        , 40
+        , 'Y'
+        , 'billt1'
+        );        
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Lily'
+        , 'May'
+        , 'Junior Backend Developer'
+        , 25
+        , 'Y'
+        , 'lil12'
+        );
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Nona'
+        , 'Lucky'
+        , 'Junior Teacher'
+        , 20
+        , 'N'
+        , 'LuckyNona'
+        ); 
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ( 'Nancy'
+        , 'Greenberg'
+        , 'Middle UI Designer'
+        , 32
+        , 'Y'
+        , 'nancy1'
+        );        
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Daniel'
+        , 'Faviet'
+        , 'Senior UX Designer'
+        , 43
+        , 'Y'
+        , 'favietD'
+        );        
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Ally'
+        , 'Austin'
+        , 'Junior UI Designer'
+        , 28
+        , 'N'
+        , 'ally1'
+        );        
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Lily'
+        , 'Chen'
+        , 'Senior Teacher'
+        , 25
+        , 'Y'
+        , 'lilychen'
+        );        
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Anna'
+        , 'Austin'
+        , 'Middle Teacher'
+        , 34
+        , 'Y'
+        , 'anna28'
+        );        
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Alexander'
+        , 'Lorentz'
+        , 'Junior Backend Developer'
+        , 25
+        , 'N'
+        , 'alex12345'
+        );        
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Ashley'
+        , 'Lorentz'
+        , 'Junior UX Designer'
+        , 18
+        , 'N'
+        , 'lorentz99'
+        );        
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Tom'
+        , 'Lucky'
+        , 'Middle Blogger'
+        , 34
+        , 'Y'
+        , 'lucky78'
+        );        
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Ashley'
+        , 'Weiss'
+        , 'Junior Blogger'
+        , 18
+        , 'N'
+        , 'weiss11'
+        );
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Tom'
+        , 'Weiss'
+        , 'Junior UI Designer'
+        , 18
+        , 'N'
+        , 'tom222'
+        );   
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Anna'
+        , 'Bloom'
+        , 'Middle UX Designer'
+        , 20
+        , 'N'
+        , 'bloom5'
+        );   
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Tom'
+        , 'Berg'
+        , 'Middle Teacher'
+        , 34
+        , 'N'
+        , 'tommy1'
+        );   
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Ashley'
+        , 'Berg'
+        , 'Senior Teacher'
+        , 37
+        , 'N'
+        , 'ash89'
+        );
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Lily'
+        , 'Weiss'
+        , 'Middle Blogger'
+        , 45
+        , 'N'
+        , 'lilyW1'
+        );   
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Anna'
+        , 'Lorentz'
+        , 'Senior UX Designer'
+        , 31
+        , 'N'
+        , 'annlo1'
+        );   
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Ashley'
+        , 'Weiss'
+        , 'Middle UX Designer'
+        , 18
+        , 'N'
+        , 'weiss1'
+        );       
+INSERT INTO Staff(firstname, lastname, position, age, has_child, username) VALUES 
+        ('Anna'
+        , 'Keren'
+        , 'Junior UX Designer'
+        , 34
+        , 'N'
+        , 'annaK1'
+        );       
+
+select *
+from Staff
+where age between 20 and 40;
+
+select *
+from Staff
+where age<30 or age>45;
+
+select firstname, lastname, age
+from Staff
+where id in (3,7,10);
+
+select *
+from Staff
+where firstname like 'A%' and lastname like '%S';
+
+
+
+
+
+
+
+
+
+
+
+
+
